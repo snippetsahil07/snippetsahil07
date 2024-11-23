eaec63ca3351276196bfb6927cd20844267aa28b
snippetsahil07 <sheksahil016@gmail.com>
2024-11-23 13:37:24 +0530

    About.js
    Signed-off-by: snippetsahil07 <sheksahil016@gmail.com>

---
+++ About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>this is government polytechnic in computer science department</h2>
+      <p>
+        Read more about us at:
+        <a href="https://www.geekfor!.org/about/" target="_blank" rel="noopener noreferrer">
+          https://www.geekfor!.org/about/
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default About;

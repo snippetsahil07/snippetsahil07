eed8a103febb7f2855094f52fbf608fc6d02a651
snippetsahil07 <sheksahil016@gmail.com>
2024-11-23 13:38:25 +0530

    Contact.js
    Signed-off-by: snippetsahil07 <sheksahil016@gmail.com>

---
+++ Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <address>
+      computer science engineering department
+    
+      <br />
+      5th & 6th Floor, Royal Kapsons, A-118,
+      <br />
+      near stedium in lingasuru
+    </address>
+  );
+}
+
+export default Contact;
+
